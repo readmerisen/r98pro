1d471fdef1353d90be1a22b93bdb17b3f9078b54
readmerisen <r3ad_me@yahoo.com>
2015-01-06 15:43:40 +0700

    Add a fully functional sidebar menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
+$(document).ready(function(){
+	$('.menuBarToggle').on('click', function(e){
+		e.preventDefault();
 
+		var menuBar = $('.menuBar');
+		var menuBarWidth = $('.menuBar').width();
+		var menuBarButton = $('#menuButton');
+
+		menuBar.toggleClass("open");
+
+		if (menuBar.hasClass("open")) {
+			menuBar.animate({
+				right: "0px"
+			});
+			menuBarButton.animate({
+				right: menuBarWidth+5,
+			}, 250);
+		} else {
+			menuBar.animate({
+				right: -menuBarWidth
+			},250);
+			menuBarButton.animate({
+				right: "1em"
+			}, 250);
+		}
+	});
+});
