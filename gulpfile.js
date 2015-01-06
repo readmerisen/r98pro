2227779300d447c1b9c7f183d8a9a9a239d98825
readmerisen <r3ad_me@yahoo.com>
2015-01-06 09:15:08 +0700

    Initial Release

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+var reload = browserSync.reload;
+
+//watch files for changes and reload
+gulp.task('serve', function(){
+	browserSync({
+		server:{
+			baseDir: 'src'
+		}
+	});
+
+	gulp.watch([
+		'*.html',
+        'scss/*.scss',
+		'css/*.css',
+		'js/*.js'], {cwd: 'src'}, reload);
+});
+
+gulp.task('imagemin', function(){
+    return gulp.src('src/resources/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('src/img'));
+});
